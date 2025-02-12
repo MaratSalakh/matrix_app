f4f99f2cfe80e4df7d6a11a33dc3385f52feeb3b
Marat Salakh <marat.salakh.dev@gmail.com>
2025-02-12 14:13:24 +0300

    create header

---
+++ src/components/ui/Header/HeaderUI.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ButtonUI } from "../ButtonUI/ButtonUI";
+import styles from "./HeaderUI.module.css";
+import { useRouter } from "next/navigation";
+
+export default function HeaderUI() {
+  const router = useRouter();
+
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.title}>Matrix</h1>
+      <ButtonUI onClick={() => router.push("/")}>Back</ButtonUI>
+    </div>
+  );
+}
