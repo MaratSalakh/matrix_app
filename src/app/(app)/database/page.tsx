d7b0a2d811826ef66f19e953162ead04e17e8a67
Marat Salakh <marat.salakh.dev@gmail.com>
2025-02-18 19:48:03 +0300

    add database

--- src/app/(app)/database/page.tsx
+++ src/app/(app)/database/page.tsx
@@ -2,46 +2,17 @@
 
 import styles from "./page.module.css";
 import { ButtonUI } from "@/components/ui/ButtonUI/ButtonUI";
-import { Daum, Root } from "@/models/responseUsers";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-
-const url = "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10";
-const options = { method: "GET", headers: { accept: "application/json" } };
-
-async function getUsers() {
-  try {
-    const response = await fetch(url, options);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
+import { useGetUsersQuery } from "@/network/apiDATA";
+import React from "react";
 
 export default function DatabasePage() {
-  const [users, setUsers] = useState<Daum[]>([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    const asyncFunc = async () => {
-      const data: Root = await getUsers();
-      setUsers(data.data.data);
-    };
-
-    asyncFunc();
-  }, []);
+  const { data: users } = useGetUsersQuery({ page: 1, limit: 20 });
 
   return (
     <div>
       <div className={styles.mainBlock}>
-        {users.map((item, i) => (
-          <ButtonUI
-            onClick={() => router.push(`database/${i}`)}
-            className={styles.button}
-            key={i}
-          >
+        {users?.data.data.map((item, i) => (
+          <ButtonUI className={styles.listItem} key={i}>
             {`${item.name.first} ${item.name.last}`}
           </ButtonUI>
         ))}
