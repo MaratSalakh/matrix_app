2ec7f48477d4fdce51e8bb6346bceb4acdcacdcd
Marat Salakh <marat.salakh.dev@gmail.com>
2025-03-02 19:14:59 +0300

    add new base url

--- src/utils/baseURLs.ts
+++ src/utils/baseURLs.ts
@@ -1,3 +1,5 @@
 export const baseURL_AI = "https://openrouter.ai/api";
 
 export const baseURL_DATA = "https://api.freeapi.app/api/v1/public/";
+
+export const baseURL_MATRIX = "https://a833410ee345ecf7.mokky.dev";
